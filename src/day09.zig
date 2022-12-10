f9d02272114fb7b4411f847bcaf97c492b649b70
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2022-12-10 10:16:32 +0200

    Solve day 9 part 2

--- src/day09.zig
+++ src/day09.zig
@@ -22,108 +22,142 @@ const Vec2 = @Vector(2, i32);
 
 const TailVisits = std.AutoHashMap(Vec2, void);
 
-const Grid = struct {
-    const Self = @This();
+fn Grid(comptime NTailKnots: usize) type {
+    assert(NTailKnots >= 1);
 
-    width: usize,
-    height: usize,
-    head: Vec2,
-    tail: Vec2,
-    tail_visits: TailVisits,
-    allocator: Allocator,
+    return struct {
+        const Self = @This();
 
-    fn init(allocator: Allocator) !Self {
-        const start = Vec2{ 0, 0 };
+        width: usize,
+        height: usize,
+        head: Vec2,
+        tail_knots: [NTailKnots]Vec2,
+        tail_visits: TailVisits,
+        allocator: Allocator,
 
-        var ret = Self{
-            .width = 5,
-            .height = 5,
-            .head = start,
-            .tail = start,
-            .tail_visits = std.AutoHashMap(Vec2, void).init(allocator),
-            .allocator = allocator,
-        };
-        try ret.mark(start);
+        fn init(allocator: Allocator) !Self {
+            const start = Vec2{ 0, 0 };
 
-        return ret;
-    }
+            var ret = Self{
+                .width = 5,
+                .height = 5,
+                .head = start,
+                .tail_knots = [_]Vec2{start} ** NTailKnots,
+                .tail_visits = std.AutoHashMap(Vec2, void).init(allocator),
+                .allocator = allocator,
+            };
+            try ret.markTail();
 
-    fn deinit(self: *Self) void {
-        self.tail_visits.deinit();
-    }
-
-    fn mark(self: *Self, coord: Vec2) !void {
-        try self.tail_visits.put(coord, {});
-    }
-
-    fn makeMove(self: *Self, dir: u8, dist: usize) !void {
-        const step = switch (dir) {
-            'U' => Vec2{ 0, 1 },
-            'D' => Vec2{ 0, -1 },
-            'L' => Vec2{ -1, 0 },
-            'R' => Vec2{ 1, 0 },
-            else => @panic("Invalid direction"),
-        };
-
-        var i: usize = 0;
-        while (i < dist) : (i += 1) {
-            self.head = self.head + step;
-
-            const head_dir = self.head - self.tail;
-            const abs_head_dir = @max(head_dir, head_dir * Vec2{ -1, -1 });
-            if (@reduce(.Or, abs_head_dir > Vec2{ 1, 1 })) {
-                // Move at most 1 step in either direction
-                const tail_move = @max(@min(head_dir, Vec2{ 1, 1 }), Vec2{ -1, -1 });
-                self.tail = self.tail + tail_move;
-                try self.mark(self.tail);
-            }
-
-            // self.print();
-        }
-    }
-
-    fn print(self: *const Self) void {
-        var min_p = self.head;
-        var max_p = self.head;
-        min_p = @min(min_p, self.tail);
-        max_p = @max(max_p, self.tail);
-
-        var keys = self.tail_visits.keyIterator();
-        while (keys.next()) |key| {
-            min_p = @min(min_p, key.*);
-            max_p = @max(max_p, key.*);
+            return ret;
         }
 
-        const width = max_p[0] - min_p[0] + 1;
-        const height = max_p[1] - min_p[1] + 1;
+        fn deinit(self: *Self) void {
+            self.tail_visits.deinit();
+        }
 
-        // Coordinate system is X left, Y up
-        const top_left = Vec2{ min_p[0], max_p[1] };
+        fn markTail(self: *Self) !void {
+            try self.tail_visits.put(self.tail_knots[NTailKnots - 1], {});
+        }
 
-        var j: i32 = 0;
-        while (j < height) : (j += 1) {
-            var i: i32 = 0;
-            while (i < width) : (i += 1) {
-                // Down is -Y
-                const pos = top_left + Vec2{ i, -j };
-                if (@reduce(.And, pos == self.head)) {
-                    dbgPrint("H", .{});
-                } else if (@reduce(.And, pos == self.tail)) {
-                    dbgPrint("T", .{});
-                } else if (self.tail_visits.contains(pos)) {
-                    dbgPrint("#", .{});
-                } else {
-                    dbgPrint(".", .{});
+        fn makeMove(self: *Self, dir: u8, dist: usize) !void {
+            const step = switch (dir) {
+                'U' => Vec2{ 0, 1 },
+                'D' => Vec2{ 0, -1 },
+                'L' => Vec2{ -1, 0 },
+                'R' => Vec2{ 1, 0 },
+                else => @panic("Invalid direction"),
+            };
+
+            var i_move: usize = 0;
+            while (i_move < dist) : (i_move += 1) {
+                self.head = self.head + step;
+
+                var target = self.head;
+                var i_tail: usize = 0;
+                while (i_tail < NTailKnots) : (i_tail += 1) {
+                    const pos = &self.tail_knots[i_tail];
+                    const target_dir = target - pos.*;
+                    const abs_target_dir = @max(target_dir, target_dir * Vec2{ -1, -1 });
+                    if (@reduce(.Or, abs_target_dir > Vec2{ 1, 1 })) {
+                        // Move at most 1 step in either direction
+                        const knot_move = @max(@min(target_dir, Vec2{ 1, 1 }), Vec2{ -1, -1 });
+                        pos.* += knot_move;
+                    }
+                    try self.markTail();
+                    target = pos.*;
                 }
-            }
-            dbgPrint("\n", .{});
-        }
-    }
 
-    fn countTailVisits(self: *const Self) usize {
-        return self.tail_visits.count();
-    }
-};
+                // self.print();
+            }
+        }
+
+        fn print(self: *const Self) void {
+            var min_p = self.head;
+            var max_p = self.head;
+
+            var keys = self.tail_visits.keyIterator();
+            while (keys.next()) |key| {
+                min_p = @min(min_p, key.*);
+                max_p = @max(max_p, key.*);
+            }
+
+            const width = max_p[0] - min_p[0] + 1;
+            const height = max_p[1] - min_p[1] + 1;
+
+            // Coordinate system is X left, Y up
+            const top_left = Vec2{ min_p[0], max_p[1] };
+
+            var j: i32 = 0;
+            while (j < height) : (j += 1) {
+                var i: i32 = 0;
+                while (i < width) : (i += 1) {
+                    // Down is -Y
+                    const pos = top_left + Vec2{ i, -j };
+                    var is_empty = true;
+                    if (@reduce(.And, pos == self.head)) {
+                        dbgPrint("H", .{});
+                        is_empty = false;
+                    } else if (NTailKnots == 1 and @reduce(.And, pos == self.tail_knots[0])) {
+                        dbgPrint("T", .{});
+                        is_empty = false;
+                    } else if (NTailKnots > 1) {
+                        knots: for (self.tail_knots) |t_pos, t_i| {
+                            if (@reduce(.And, pos == t_pos)) {
+                                dbgPrint("{d}", .{t_i + 1});
+                                is_empty = false;
+                                break :knots;
+                            }
+                        }
+                    }
+                    if (is_empty) {
+                        if (self.tail_visits.contains(pos)) {
+                            dbgPrint("#", .{});
+                        } else {
+                            dbgPrint(".", .{});
+                        }
+                    }
+                }
+                dbgPrint("\n", .{});
+            }
+        }
+
+        fn countTailVisits(self: *const Self) usize {
+            return self.tail_visits.count();
+        }
+
+        fn makeMoves(self: *Self) !void {
+            var lines = std.mem.split(u8, input_txt, line_ending);
+            while (lines.next()) |line| {
+                var parts = std.mem.split(u8, line, " ");
+
+                const dir = parts.next().?[0];
+                const dist = try std.fmt.parseUnsigned(usize, parts.next().?, 10);
+
+                try self.makeMove(dir, dist);
+            }
+        }
+    };
+}
 
 pub fn main() !void {
     // Let's not use the arena allocator to get comfortable with defer dtors
@@ -137,24 +171,25 @@ pub fn main() !void {
 
     const part = try getPart(allocator);
 
-    var grid = try Grid.init(allocator);
-    defer grid.deinit();
-
-    var lines = std.mem.split(u8, input_txt, line_ending);
-    while (lines.next()) |line| {
-        var parts = std.mem.split(u8, line, " ");
-
-        const dir = parts.next().?[0];
-        const dist = try std.fmt.parseUnsigned(usize, parts.next().?, 10);
-
-        try grid.makeMove(dir, dist);
-    }
-
-    grid.print();
-
     if (part == 1) {
+        var grid = try Grid(1).init(allocator);
+        defer grid.deinit();
+
+        try grid.makeMoves();
+
+        grid.print();
+
         try stdout.print("Tail visited {d} positions", .{grid.countTailVisits()});
-    } else {}
+    } else {
+        var grid = try Grid(9).init(allocator);
+        defer grid.deinit();
+
+        try grid.makeMoves();
+
+        grid.print();
+
+        try stdout.print("Tail visited {d} positions", .{grid.countTailVisits()});
+    }
 
     // Make sure we end with a newline
     try stdout.print("\n", .{});
